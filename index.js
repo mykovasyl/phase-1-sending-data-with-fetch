0480da6d37305ba8423ddb2cb4e94eedf1e2080a
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-02-17 19:18:49 -0500

    commit tree a154a2624842258754b7ff6735e957076abcfc58

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+
+function submitData(userName, userEmail) {
+  const list = document.querySelector('body')
+  return fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  })
+  .then(resp => resp.json())
+  .then(function (data) {
+    list.append(data.id);
+  })
+  .catch(function (err) {
+    list.append(err.message)
+  })
+}
